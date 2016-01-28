421d00c5a9befd8fa584a44d8c3404acbed2ee62
Peter Azmanov <peter.azmanov@transas.com>
2016-01-28 20:21:36 +0300

    increase version

--- byteplay.py
+++ byteplay.py
@@ -17,7 +17,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-__version__ = '0.3'
+__version__ = '0.4'
 __all__ = [
     'opmap',
     'opname',
